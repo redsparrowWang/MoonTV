d783a4271aaff8eaf0c143a5f4b2f135b763db66
redsparrowWang <wang@itsherpa.com>
2025-09-02 11:37:46 +0900

    Delete functions/index.ts

--- functions/index.ts
+++
@@ -1,6 +0,0 @@
-// functions/index.ts
-import type { PagesFunction } from '@cloudflare/workers-types';
-
-export const onRequest: PagesFunction = (context) => {
-  return new Response("Hello, Pages Functions!");
-}
